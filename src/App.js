d97ff18bd5784de13e797e8a0926e75024ea2c7d
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-07-29 00:09:53 +0530

    my weatherapp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Todo from "./components/todoreact/todo";
+import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
+
+  //  `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
+
+// api.openweathermap.org/data/2.5/weather?q=pune&appid=8bc54f260ca68f38692d68e43f5dd9ed
